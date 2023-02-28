f63db661395d64006cc15a1ada8a9ef8bc0b0318
mauricio Tovarich <mau141914@gmail.com>
2023-02-28 15:38:55 -0600

    Cajero_prueba
    Prueba

--- Cajero_Mau/script.js
+++ Cajero_Mau/script.js
@@ -1,34 +1,33 @@
-function hideIntroBienvenido() {
-  document.querySelector('.introBienvenido').style.display = 'none';
-}
+
+let usuarios = [
+  { usuario: 'Mauricio', password: 'Batman', saldo: 100 },
+  { usuario: 'Vale', password: 'pass2', saldo: 20 },
+  { usuario: 'Laura', password: 'pass3', saldo: 200 },
+];
 
 
-function showBienvenido() {
-  document.querySelector('.introBienvenido').style.display = 'block';
-
-}
-
+document.querySelector('.introBienvenido').style.display = 'none';
 document.querySelector('#btnSaldo').style.display = 'none';
-document.querySelector('#Ingresar').style.display = 'none';
+document.querySelector('#btnIngresar').style.display = 'none';
 document.querySelector('#btnRetirar').style.display = 'none';
 
 
 
-let usuarios = [
-  { usuario: 'Mariana', password: 'pass1', saldo: 100 },
-  { usuario: 'Vale', password: 'pass2', saldo: 20 },
-  { usuario: 'Laura', password: 'pass3', saldo: 200 },
-];
+
+document.querySelector('.entrada').onclick = () => {
 function Addprofile () {
-    let name = document.querySelector('#name').value;
-    let pass = document.querySelector('#pass').value;
-    let saldo = document.querySelector('#saldo').value;
-    if (name === usuarios.inName && pass === usuarios.pass) {
+    let usuarios = document.querySelector('.inName').value;
+    let pass = document.querySelector('.passwor').value;
+    let saldo = usuarios.dalso;/* document.querySelector('.saldo').value; */
+    if (usuarios === usuarios.inName && pass === usuarios.pass) {
         showBienvenido();
         document.querySelector('.introBienvenido').innerHTML = `Bienvenido ${usuarios.inName}, tu saldo es de ${usuarios.saldo}`;
     } else {
         alert('Usuario o contraseña incorrectos');
     }
 }
+}
+function showBienvenido() {
+  document.querySelector('.introBienvenido').style.display = 'block';
 
-
+}
